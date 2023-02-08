7cfd06a50497e749507ee64cb2cfea94b08e7251
shahidmd78 <shahidm@gmail.com>
2023-02-07 21:28:43 -0500

    network

--- main.tf
+++ main.tf
@@ -34,3 +34,22 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
 }
+
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
