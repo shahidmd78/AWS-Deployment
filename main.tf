507788da3c38ae51e4d5d24e64e3358aca5f8be1
shahidmd78 <shahidm@gmail.com>
2023-02-07 20:33:23 -0500

    new deployment

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  cloud {
+    organization = "Bisma"
+
+    workspaces {
+      name = "doger"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.53.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-233333"
+
+  tags = {
+    Name        = "My bucket22"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
