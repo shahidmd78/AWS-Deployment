1fa73dc3961842b6fd65149e96edae72777beffb
shahidmd78 <shahidm@gmail.com>
2023-02-09 13:46:25 -0500

    security-group

--- main.tf
+++ main.tf
@@ -56,3 +56,31 @@ resource "aws_subnet" "my_subnet" {
 resource "aws_ec2_transit_gateway" "example" {
   description = "example"
 }
+
+#######Security Group
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
+     }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
