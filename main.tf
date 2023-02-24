bbd5eabde20f55359784b261afe6884ad24a748f
shahidmd78 <shahidm@gmail.com>
2023-02-24 12:02:05 -0500

    change-1

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "aws_instance" "terraform-ec2-instance-1" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
    tags = {
-    Name        = "My EC2 Instance-1"
+    Name        = "My EC2 Instance-2"
     Environment = "Production"
   }
  subnet_id = aws_subnet.my_subnet.id
